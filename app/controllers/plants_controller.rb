6c8f9b47436240b7c3fdfe493aa7425de395cb19
yeohoon yoon <yhoon.yoon@gmail.com>
2022-03-15 22:15:46 -0400

    complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,6 @@
 class PlantsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   # GET /plants
   def index
     plants = Plant.all
@@ -8,19 +9,45 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
   # POST /plants
   def create
-    plant = Plant.create(plant_params)
+    plant = Plant.create!(plant_params)
     render json: plant, status: :created
+  end
+
+  def update
+    plant = find_plant
+    plant.update!(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
 
+
+
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_unprocessable_entity(invalid)
+    render json:{errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def render_not_found
+    render json:{errors: "Not found"}, status: :not_found
   end
 end
